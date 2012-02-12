1d44f71668c1239c69bb8f55b387b995e0c7828a
nando <toni.andrianaht@gmail.com>
2012-02-12 16:16:17 -0600

    news model on dev

--- news/models.py
+++ news/models.py
@@ -1,7 +1,18 @@
 from django.db import models
+from datetime import datetime
+from django.contrib.auth.models import User
 
 # Create your models here.
-
 class News(models.Model):
-    title = models.CharField(max_length=100)
-    content = models.TextField()
+    title = models.CharField(max_length = 50)
+    body = models.TextField()
+    creator = models.ForeignKey(User)
+    creation_date = models.DateTimeField(default=datetime.now)
+    published_date = models.DateTimeField(default=datetime.now, null=True, blank=True)
+    
+    
+    def __unicode__(self):
+        return self.title;
+    
+    class Meta(object):
+        verbose_name_plural = "News"

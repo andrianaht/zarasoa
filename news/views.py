e85e79ba48bfc86eb2f027c7929a3c6383db7dfd
nando <toni.andrianaht@gmail.com>
2012-02-12 16:19:08 -0600

    news model on dev

--- news/views.py
+++ news/views.py
@@ -1,1 +1,17 @@
 # Create your views here.
+from News.models import News
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+
+
+def index(request):
+    list_of_news = News.objects.all().order_by('-published_date')[:5]
+    
+    context = {
+        'news': list_of_news,
+    }
+    return render_to_response(
+        'news.html',
+        context,
+        context_instance = RequestContext(request),)
+    
